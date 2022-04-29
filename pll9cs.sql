773ea6292a4fc266eb8f41109b007ef7d0cd1967
h-zsanett <hlatkyzsanett@gmail.com>
2022-04-29 16:18:54 +0200

    Database file updated

--- pll9cs.sql
+++ pll9cs.sql
@@ -1,47 +1,118 @@
-DROP DATABASE `pll9cs`;
-CREATE DATABASE `pll9cs` CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 29, 2022 at 04:14 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
-USE `pll9cs`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `users` (
-  `id` int(10) unsigned     NOT NULL auto_increment,
-  `vezeteknev` varchar(45) NOT NULL default '',
-  `keresztnev` varchar(45)     NOT NULL default '',
-  `username` varchar(12) NOT NULL default '',
-  `passwd` varchar(40)      NOT NULL default '',
-  PRIMARY KEY  (`id`)
-)
-ENGINE = MYISAM
-CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-INSERT INTO `users` (`id`,`vezeteknev`,`keresztnev`,`username`,`passwd`) VALUES 
- (1,'Családnév1','Keresztnév1','Login1',sha1('login1')),
- (2,'Családnév2','Keresztnév2','Login2',sha1('login2')),
- (3,'Családnév3','Keresztnév3','Login3',sha1('login3')),
- (4,'Családnév4','Keresztnév4','Login4',sha1('login4')),
- (5,'Családnév5','Keresztnév5','Login5',sha1('login5')),
- (6,'Családnév6','Keresztnév6','Login6',sha1('login6')),
- (7,'Családnév7','Keresztnév7','Login7',sha1('login7')),
- (8,'Családnév8','Keresztnév8','Login8',sha1('login8')),
- (9,'Családnév9','Keresztnév9','Login9',sha1('login9')),
- (10,'Családnév10','Keresztnév10','Login10',sha1('login10')),
- (11,'Családnév11','Keresztnév11','Login11',sha1('login11')),
- (12,'Családnév12','Keresztnév12','Login12',sha1('login12'));
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pll9cs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
 
 CREATE TABLE `messages` (
-  `id` int(10) unsigned     NOT NULL auto_increment,
-  `cimzett` varchar(40) NOT NULL default '',
-  `usernev` varchar(40)     NOT NULL default '',
-  `email` varchar(50) NOT NULL default '',
-  `idopont` datetime  NOT NULL default '2000-01-01 00:00:00',
-  `uzenet` varchar(2000)      NOT NULL default '',
-  PRIMARY KEY  (`id`)
-)
-ENGINE = MYISAM
-CHARACTER SET utf8 COLLATE utf8_general_ci;
+  `id` int(10) UNSIGNED NOT NULL,
+  `cimzett` varchar(40) NOT NULL DEFAULT '',
+  `usernev` varchar(40) NOT NULL DEFAULT '',
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `idopont` datetime NOT NULL DEFAULT current_timestamp(),
+  `uzenet` varchar(2000) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO `messages` (`id`,`cimzett`,`usernev`,`email`,`idopont`,`uzenet`) VALUES 
- (1,'Családnév1','Login1','Vendég','2022-04-29 11:00:07','Szia! Ez az első üzenetem, nem vagyok belépve csak vengégként írok!'),
- (2,'Családnév2','Login2','Login1','2022-04-29 12:22:51','Beléptem! Vajon megérkezik-e az üzenet'),
- (3,'Családnév3','Login3','Login1','2022-04-29 13:13:13','Itt is be vagyok lépve, ellenőrizni kell az időpontok helyességét az adatbázisban');
+--
+-- Dumping data for table `messages`
+--
 
+INSERT INTO `messages` (`id`, `cimzett`, `usernev`, `email`, `idopont`, `uzenet`) VALUES
+(1, 'Családnév1', 'Login1', 'Vendég', '2022-04-29 11:00:07', 'Szia! Ez az első üzenetem, nem vagyok belépve csak vendégként írok!'),
+(2, 'Családnév2', 'Login2', 'Login1', '2022-04-29 12:22:51', 'Beléptem! Vajon megérkezik-e az üzenet'),
+(3, 'Családnév3', 'Login3', 'Login1', '2022-04-29 13:13:13', 'Itt is be vagyok lépve, ellenőrizni kell az időpontok helyességét az adatbázisban'),
+(7, 'Józsi', 'Login4', 'akarmi@kukac.hu', '2022-04-29 15:17:03', 'uzenetuziuziuzi'),
+(13, 'Lacika', 'Vendég', 'nagylaci@laci.hu', '2022-04-29 16:04:27', 'LaciLaciLaciLaciLaciLaciLaciLaci');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `vezeteknev` varchar(45) NOT NULL DEFAULT '',
+  `keresztnev` varchar(45) NOT NULL DEFAULT '',
+  `username` varchar(12) NOT NULL DEFAULT '',
+  `passwd` varchar(40) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `vezeteknev`, `keresztnev`, `username`, `passwd`) VALUES
+(1, 'Családi_1', 'Utónév_1', 'Login1', 'd4b90f2dfafc736205a98bf3ae6541431bc77d8e'),
+(2, 'Családi_2', 'Utónév_2', 'Login2', '6cf8efacae19431476020c1e2ebd2d8acca8f5c0'),
+(3, 'Családi_3', 'Utónév_3', 'Login3', 'df4d8ad070f0d1585e172a2150038df5cc6c891a'),
+(4, 'Családi_4', 'Utónév_4', 'Login4', 'b020c308c155d6bbd7eb7d27bd30c0573acbba5b'),
+(5, 'Családi_5', 'Utónév_5', 'Login5', '9ab1a4743b30b5e9c037e6a645f0cfee80fb41d4'),
+(6, 'Családi_6', 'Utónév_6', 'Login6', '7ca01f28594b1a06239b1d96fc716477d198470b'),
+(7, 'Családi_7', 'Utónév_7', 'Login7', '41ad7e5406d8f1af2deef2ade4753009976328f8'),
+(8, 'Családi_8', 'Utónév_8', 'Login8', '3a340fe3599746234ef89591e372d4dd8b590053'),
+(9, 'Családi_9', 'Utónév_9', 'Login9', 'c0298f7d314ecbc5651da5679a0a240833a88238'),
+(10, 'Családi_10', 'Utónév_10', 'Login10', 'a477427c183664b57f977661ac3167b64823f366'),
+(11, 'Családi_11', 'Utónév_11', 'Login11', '6c7e2151d33968a02198f7e2073b1d7b9ee65ed9'),
+(12, 'Családi_12', 'Utónév_12', 'Login12', '0722b3651be10eeb8df39cced958b74a98d18ce3'),
+(13, 'Kiss', 'Miska', 'Misi', '0691ba960926480ea6a83205e451de1d30d8e8f1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
